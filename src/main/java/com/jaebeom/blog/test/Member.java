6fe1644a132e099bf32f6464923e974473c6fef3
kimjaebeom98 <woqjadl6487@naver.com>
2023-11-27 23:28:09 +0900

    http 실습

---
+++ src/main/java/com/jaebeom/blog/test/Member.java
@@ -0,0 +1,36 @@
+package com.jaebeom.blog.test;
+
+public class Member {
+	
+	private int id;
+	private String username;
+	private int password;
+	private String email;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getPassword() {
+		return password;
+	}
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	
+	
+}
