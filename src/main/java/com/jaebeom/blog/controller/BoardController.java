d5258b1040a6b69665b471f1cb5780c9199613f8
kimjaebeom98 <woqjadl6487@naver.com>
2024-01-14 22:27:19 +0900

    글 목록 불러오기

--- src/main/java/com/jaebeom/blog/controller/BoardController.java
+++ src/main/java/com/jaebeom/blog/controller/BoardController.java
@@ -1,16 +1,27 @@
 package com.jaebeom.blog.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.jaebeom.blog.config.auth.PrincipalDetail;
+import com.jaebeom.blog.model.Board;
+import com.jaebeom.blog.service.BoardService;
 
 @Controller
 public class BoardController {
 
+	@Autowired
+	private BoardService boardService;
+	
 	@GetMapping({"", "/"})
-	public String index() {
+	public String index(Model model) {
+		List<Board> boards = boardService.글목록();
+		model.addAttribute("boards", boards);
 		return "index";
 	}
 	
--- src/main/java/com/jaebeom/blog/service/BoardService.java
+++ src/main/java/com/jaebeom/blog/service/BoardService.java
@@ -1,5 +1,7 @@
 package com.jaebeom.blog.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -23,5 +25,9 @@ public class BoardService {
 		board.setCount(0);
 		board.setUser(user);
 		boardRepository.save(board);
+	}
+	
+	public List<Board> 글목록(){
+		return boardRepository.findAll();
 	}
 }
